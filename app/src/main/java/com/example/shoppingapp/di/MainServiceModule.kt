eafe7a0f66d556493658471676f8bbddc4c6302d
SB <seungbae2@gmail.com>
2023-04-05 23:46:22 +0900

    Retrofit setup

---
+++ app/src/main/java/com/example/shoppingapp/di/MainServiceModule.kt
@@ -0,0 +1,18 @@
+package com.example.shoppingapp.di
+
+import com.example.shoppingapp.MainService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object MainServiceModule {
+
+    @Provides
+    @Singleton
+    fun provideMainService(retrofit: Retrofit) : MainService = retrofit.create(MainService::class.java)
+}
