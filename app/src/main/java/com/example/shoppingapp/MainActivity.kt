442e6afe89881aed1cd6142606d8d24e26df1954
SB <seungbae2@gmail.com>
2023-04-09 23:07:06 +0900

    리스트 출력

--- app/src/main/java/com/example/shoppingapp/MainActivity.kt
+++ app/src/main/java/com/example/shoppingapp/MainActivity.kt
@@ -2,21 +2,39 @@ package com.example.shoppingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.shoppingapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val adapter by lazy { ListAdapter() }
+    private val viewModel : MainViewModel by viewModels()
+
+    private val adapter by lazy { PagingListAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).apply {
             setContentView(root)
             recyclerView.adapter = adapter
+        }
+
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        lifecycleScope.launch {
+            viewModel.pagingData.collectLatest {
+                if (it != null) {
+                    adapter.submitData(lifecycle, it)
+                }
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/shoppingapp/MainViewModel.kt
@@ -0,0 +1,38 @@
+package com.example.shoppingapp
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.example.shoppingapp.model.ListItem
+import com.example.shoppingapp.remote.repository.MainRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val mainRepository: MainRepository
+) : ViewModel(){
+
+    private val _pagingData = MutableStateFlow<PagingData<ListItem>?>(null)
+    val pagingData : StateFlow<PagingData<ListItem>?> = _pagingData
+
+    init {
+        getList()
+    }
+
+    private fun getList() {
+        viewModelScope.launch {
+            mainRepository.loadList()
+                .cachedIn(this)
+                .collectLatest { list->
+                    _pagingData.value = list
+
+                }
+        }
+    }
+}
